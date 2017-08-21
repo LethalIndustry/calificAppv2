ad5eca051a3ba888193f75aa455f44de69066acf
LethalIndustry <josu3rg@hotmail.com>
2017-08-21 17:24:29 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,19 +4,43 @@ $(document).on('deviceready',function(){
        curp = $("#curp").val();
         longitud = curp.length;
         if(curp==""){
-            //alert("Debe llenar el campo CURP");
-            navigator.notification.alert('You are the winner!',alertDismissed,'Game Over', 'Done');
+            navigator.notification.alert('Debe llenar el campo CURP',alertDismissed,'Error', 'Ok');
             return false;
         }else{
             if(longitud!=18){
-                alert("El campo CURP debe contener 18 caracteres");
+                navigator.notification.alert('El campo CURP debe contener 18 caracteres',alertDismissed,'Error', 'Ok');
                 return false;
             }else{
-                alert("Siguiente pagina");
+                $.mobile.changePage("#menu", {transition: "slide", changeHash: false });
             }
         } 
     })  
 });
+
+document.addEventListener("backbutton", function(e){
+    if($.mobile.activePage.is('#index')){
+        /* 
+         Event preventDefault/stopPropagation not required as adding backbutton
+          listener itself override the default behaviour. Refer below PhoneGap link.
+        */
+        //e.preventDefault();
+
+        navigator.notification.confirm(
+            "Do you want to exit the app?",
+            function (button) {
+              if (button==2) {
+                navigator.app.exitApp();
+              }
+            }
+            ,
+            "EXIT",
+            ["Cancelar","Ok"]
+        );
+    }
+    else {
+        navigator.app.backHistory()
+    }
+}, false);
 
 function alertDismissed() {
     // do something
